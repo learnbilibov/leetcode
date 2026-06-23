8f1f87f415e5faa88b49be08051993a75d721f14
Roma Bilibov <romabilibov@gmail.com>
2026-06-23 23:16:06 +0200

    03 - 1. Two Sum

---
+++ src/exercices/00.template.ts
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+function task(){
+    
+}
+
+export function init(): void{
+    const exercices = [
+
+    ];
+
+
+    for(let ex of exercices){
+        console.log(ex)
+        console.log()
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // import { init } from './exercices/01.ContainsDuplicate'
-
-import { init } from './exercices/02.ValidAnagram'
+// import { init } from './exercices/02.ValidAnagram'
+import { init } from './exercices/03.TwoSum'
 
 init()
